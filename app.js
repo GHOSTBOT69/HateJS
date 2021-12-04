6e4bdca3509e66f2e1a9030fe16a98250c7dfcbe
fnixdev <luisgatn000@gmail.com>
2021-12-04 07:37:35 -0300

    ++

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
-const TelegramBot = require('node-telegram-bot-api');
 require('dotenv').config({ path: 'process.env' })
+import { Telegraf } from 'telegraf'
 
-const bot = new TelegramBot(process.env.BOT_TOKEN, {polling: true});
+// Client 
+const bot = new Telegraf(process.env.BOT_TOKEN)
 
-bot.onText(/\/echo (.+)/, (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
+bot.command('quit', (ctx) => {
+  // Explicit usage
+  ctx.telegram.leaveChat(ctx.message.chat.id)
 
-  const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
-
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, resp);
-});
+  // Using context shortcut
+  ctx.leaveChat()
+})
