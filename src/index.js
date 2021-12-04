3b48c24e94a0a7eb3c6941f29d15b7d2470c471c
fnixdev <luisgatn000@gmail.com>
2021-12-04 14:13:05 -0300

    ++

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 require('dotenv').config({ path: 'process.env' })
 const { Telegraf } = require('telegraf')
-const { fetchJson, fetchText } = require('/lib/fetcher')
 const axios = require('axios')
 const nekolife = require('nekos.life');
 const neko = new nekolife();
@@ -11,38 +10,21 @@ const bot = new Telegraf(process.env.BOT_TOKEN)
 
 // Comandos
 
-let nekos = fetchJson(`https://nekos.life/api/v2/img/neko`, {method: 'get'})
-
 console.log(nekos)
 
-bot.command('teste', ctx => {
-    console.log(ctx.from)
-    let testMessage = `maconha`;
-    bot.telegram.sendMessage(ctx.chat.id, testMessage, {
-        reply_markup: {
-            inline_keyboard: [
-                [{
-                        text: "nekos",
-                        callback_data: 'neko'
-                    },
-                    {
-                        text: "teste 2",
-                        url: 'https://github.com/'
-                    }
-                ],
+async function nekof() {
+    const GIF = await neko.sfw.neko();
+}
 
-            ]
-        }
-    })
-})
+nekof();
 
-
-bot.action('neko', ctx => {
+bot.command('neko', ctx => {
     bot.telegram.sendPhoto(ctx.chat.id, {
-        source: "nada"
+        source: GIF.url
     })
 
 })
+
 
 const startBot = async () => {
     try {
