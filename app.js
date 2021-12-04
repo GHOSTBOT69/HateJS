4c455ccdacf0a88688e35e0c806fbde7b0e1e7d8
fnixdev <luisgatn000@gmail.com>
2021-12-04 07:45:04 -0300

    ++

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ import { Telegraf } from 'telegraf'
 // Client 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
-bot.command('quit', (ctx) => {
-  // Explicit usage
-  ctx.telegram.leaveChat(ctx.message.chat.id)
-})
+bot.start((ctx) => ctx.reply("Maconha"))
+
+bot.launch()
