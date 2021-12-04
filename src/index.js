9f1844aabc4eed69ffc744b658a7aba51d8b3ece
fnixdev <luisgatn000@gmail.com>
2021-12-04 08:08:30 -0300

    ++

--- src/index.js
+++ src/index.js
@@ -8,6 +8,46 @@ const bot = new Telegraf(process.env.BOT_TOKEN)
 bot.start((ctx) => ctx.reply("Maconha"))
 
 
+bot.hears('animals', ctx => {
+    console.log(ctx.from)
+    let animalMessage = `great, here are pictures of animals you would love`;
+    ctx.deleteMessage();
+    bot.telegram.sendMessage(ctx.chat.id, animalMessage, {
+        reply_markup: {
+            inline_keyboard: [
+                [{
+                        text: "dog",
+                        callback_data: 'dog'
+                    },
+                    {
+                        text: "cat",
+                        callback_data: 'cat'
+                    }
+                ],
+
+            ]
+        }
+    })
+})
+
+//method that returns image of a dog
+
+bot.action('dog', ctx => {
+    bot.telegram.sendPhoto(ctx.chat.id, {
+        source: "res/dog.jpeg"
+    })
+
+})
+
+//method that returns image of a cat 
+
+bot.action('cat', ctx => {
+    bot.telegram.sendPhoto(ctx.chat.id, {
+        source: "res/cat.jpeg"
+    })
+
+})
+
 const startBot = async () => {
     try {
         await bot.launch()
