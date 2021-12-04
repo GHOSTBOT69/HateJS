86adc1312872777a8c6065de411ffc8b6f0f5a4b
fnixdev <luisgatn000@gmail.com>
2021-12-04 02:17:53 -0300

    ++

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const TelegramBot = require('node-telegram-bot-api');
+require('dotenv-safe').config();
 
-const token = '5033031418:AAE6qOdTV4kJP6WaOz8ZCJctt8IlSpMqn-w';
+const token = process.env.BOT_TOKEN;
 
-const bot = new TelegramBot(token, { polling: true });
+const bot = new TelegramBot(token, {polling: true});
 
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
 
-console.log(bot);
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp);
+});
