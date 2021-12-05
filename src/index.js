9dcc3d47c8a1d7990d250d9c32e0013c83c86387
fnixdev <luisgatn000@gmail.com>
2021-12-04 23:44:13 -0300

    ++

--- src/index.js
+++ src/index.js
@@ -37,6 +37,8 @@ bot.action('nekos', ctx =>
     })
 );
 
+
+
 bot.action('wall', ctx => {
   axios.get('https://nekos.life/api/v2/img/wallpaper')
     .then(res => {
@@ -47,7 +49,39 @@ bot.action('wall', ctx => {
       console.log(e);
     })
 })
+// teste 2
 
+bot.command('wallpaper', ctx => {
+let testMessage = `Apenas teste`;
+axios.get('https://nekos.life/api/v2/img/wallpaper')
+    .then(res => {
+      let las = res.data.url
+bot.telegram.sendPhoto(ctx.chat.id, las, {
+        caption: testMessage,
+        reply_markup: {
+            inline_keyboard: [
+                [
+                    {
+                        text: "Proximo",
+                        callback_data: 'walledit'
+                    }
+                ],
+            ]
+        }
+    })
+})
+})
+
+bot.action('walledit', ctx => {
+  axios.get('https://nekos.life/api/v2/img/wallpaper')
+    .then(res => {
+      let lass = res.data.url
+      bot.telegram.ExtraEditMessageMedia(ctx.msg.id, lass)
+    })
+})
+
+
+// teste 
 bot.command('boobs', ctx => {
     axios.get('https://nekos.life/api/v2/img/boobs')
     .then(res => { 
