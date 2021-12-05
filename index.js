5b4ff511f0df93fa9621ba4ec8c5c2c7225e95e4
fnixdev <luisgatn000@gmail.com>
2021-12-05 20:49:03 -0300

    ++

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const scathach = new scat();
 bot.command('oral', ctx => {
          let orall = scathach.sex.oral()
          let lav = orall.url
-         ctx.sendMessage(ctx.chat.id, lav)
+         bot.telegram.sendMessage(ctx.chat.id, lav)
     }).catch(e => {
          console.log(e);
    })
