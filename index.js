e839cd3d712507f274a7cf031c1ecddedcaf019e
fnixdev <luisgatn000@gmail.com>
2021-12-05 21:12:04 -0300

    ++

--- index.js
+++ index.js
@@ -18,6 +18,12 @@ const scathach = new scat();
 // Comandos
 
 bot.command('kek', ctx => {
+         scathach.nsfw.twitter().then(scathach => {bot.telegram.sendMessage(ctx.chat.id ,scathach.url);});
+    }).catch(e => {
+         console.log(e);
+   })
+
+bot.command('twitter', ctx => {
          scathach.nsfw.jav().then(scathach => {bot.telegram.sendMessage(ctx.chat.id ,scathach.url);});
     }).catch(e => {
          console.log(e);
